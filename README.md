559eca0148a03a0066a9470095744192c1171ff5
StefanSchrass <stefan.schrass@gmail.com>
2017-11-13 10:48:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+dfgdfg
+dfg
+
+
+
+
+[comment]: <{gav-dependency-start}>
+[Maven Dependency](https://www.google.com "gav-readmemd-maven-plugin")
+---
+```
+<dependency>   
+    <groupId>de.sschrass.maven</groupId> 
+    <artifactId>gav-readmemd-maven-plugin</artifactId> 
+    <version>1.0.0-SNAPSHOT</version>
+</dependency>
+```  
+[comment]: <{gav-dependency-end}>
+
+
+
+
+
+
+
+
+
+
+
+
+[comment]: <{gav-dependency-start}>
+[Gav-ReadmeMd-Maven-Plugin](https://www.google.com "Google's Homepage")
+```
+<dependency>
+    <groupId>de.sschrass.maven</groupId>
+    <artifactId>gav-readmemd-maven-plugin</artifactId>
+    <version>1.0.0-SNAPSHOT</version>
+</dependency>
+```
+[comment]: <{gav-dependency-end}>
+dfg
+dg
+dg
---
+++ src/main/java/GavReadmeMojo.java
@@ -0,0 +1,96 @@
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@ParametersAreNonnullByDefault
+@Mojo(name = "GavReadme", defaultPhase = LifecyclePhase.INSTALL)
+public class GavReadmeMojo extends AbstractMojo {
+    private Boolean encounteredStart = false;
+    @Parameter
+    private File readmemd;
+    @Parameter
+    private String groupId;
+    @Parameter
+    private String artifactId;
+    @Parameter
+    private String version;
+
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Updating file: " + readmemd.getAbsolutePath());
+        writeReadme(gavReadmeFromContent(contentFromFile(readmemd)));
+    }
+
+    private void writeReadme(final String readme) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(readmemd.getAbsolutePath()))) {
+            writer.write(readme);
+        }
+        catch (IOException e) {
+            getLog().error("could not write to file " + readmemd.getName(), e);
+        }
+
+    }
+
+    private String gavReadmeFromContent(String content) {
+        Pattern dependencyPattern = Pattern.compile("\\[comment\\]: \\<\\{gav-dependency-start\\}\\>.*\\[comment\\]: \\<\\{gav-dependency-end\\}\\>", Pattern.DOTALL);
+        Matcher dependencyMatcher = dependencyPattern.matcher(content);
+
+        if (dependencyMatcher.find()) {
+            content = dependencyMatcher.replaceAll(gav());
+        }
+
+        Pattern placeholderPattern = Pattern.compile("\\[comment\\]: \\<\\{gav-placeholder\\}\\>");
+        Matcher placeholderMatcher = placeholderPattern.matcher(content);
+
+        if (placeholderMatcher.find()) {
+            content = placeholderMatcher.replaceAll(gav());
+        }
+
+
+        return content;
+    }
+
+    private String contentFromFile(final File readmemd) {
+        String content;
+        try (Stream<String> lines = linesFromFile(readmemd)) {
+            content = lines.collect(Collectors.joining("\n"));
+        }
+
+        return content;
+    }
+
+    private String gav() {
+        return "[comment]: <{gav-dependency-start}>" + "\n" +
+                "[Gav-ReadmeMd-Maven-Plugin](https://www.google.com \"Google's Homepage\")\n" +
+                "```\n" +
+                "<dependency>\n" +
+                "    <groupId>" + groupId + "</groupId>\n" +
+                "    <artifactId>" + artifactId + "</artifactId>\n" +
+                "    <version>" + version + "</version>\n" +
+                "</dependency>\n" +
+                "```\n" +
+                "[comment]: <{gav-dependency-end}>";
+    }
+
+    private Stream<String> linesFromFile(final File file) {
+        Stream<String> lines = Stream.empty();
+        try { lines = Files.lines(Paths.get(file.getAbsolutePath())); }
+        catch (IOException e) { getLog().error("could not read " + file.getName(), e); }
+
+        return lines;
+    }
+}
